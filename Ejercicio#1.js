96e2876b8982dc605f46e2da1431fa7cfcd9abaa
SandraMejia <samimej13@gmail.com>
2023-06-15 22:52:10 -0500

    tercera modificacion ejercicio #1

--- Ejercicio#1.js
+++ Ejercicio#1.js
@@ -1,14 +1,14 @@
-const arrayPal = [1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99,101,111,121,131,141,151,161,171,181,191]
-const numero = 33;
-
 function nex_Pal(numero) {
-    let index = arrayPal.indexOf(numero);
-        if (index <= arrayPal.length) {
-        let siguiente = arrayPal[index + 1];
-        console.log(`El siguiente palindromo después de ${numero} es: ${siguiente}`);
-    } else {
-        console.log(`No hay número palindromo después de ${numero}`);
-    }
+  while (numero >0) {
+    numero++;
+    const numeroString = numero.toString();
+    let numreverse = numeroString.split("").reverse().join("");
+    if (numeroString === numreverse) {
+      return numero;
+    } 
+    
+  }
+  console.log("El numero debe ser un entero positivo")
 }
 
-nex_Pal(numero)
+console.log(nex_Pal(43));
